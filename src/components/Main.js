6654650c9998de0c3c267d3fbe40282d74801d98
Li <liyajie02@internal.baidu.com>
2017-07-28 21:03:44 +0800

    require json error

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,27 +4,48 @@ require('styles/App.css');
 import React from 'react';
 
 //获取图片相关数据
-let imageDatas = require('../data/imageDatas.json');
-
+var imageDatas = require('../data/imageDatas.json');
+alert(imageDatas[0]);
 //利用自执行函数将图片文件名转换为url
 imageDatas = (function genImageDatas(imageArr){
-	for (var i = 0, j = imageArr.length; i<j ; i++){
+	//alert(imageArr[1].fileName);
+	for (var i=0,j=imageArr.length; i<j ; i++){
+		alert('111');
 		var singleImageData = imageArr[i];
 		singleImageData.imageUrl = require('../images/' + singleImageData.fileName);
 		imageArr[i] = singleImageData;
-	}
+		
+	};
 	return imageArr;
 })(imageDatas);
 
+class ImageFigure extends React.Component{
+	render(){
+		<figure>
+			<img src={this.props.data.imageUrl}
+				 alt={this.props.data.title}
+			/>
+			<figcaption>
+				<h2>{this.props.data.title}</h2>
+			</figcaption>
+		</figure>
+	}
+};
+
 class AppComponent extends React.Component {
   render() {
+  	var controllerUnits = [],
+  		imgFigures = [];
+  	for(var item in imageDatas){
+  	  imgFigures.push(<ImageFigure data={item}/>);
+  	};
     return (
     	<section className="stage">
     		<section className="img-sec">
-
+    			{imgFigures}
     		</section>
     		<nav className="control-nav">
-
+				{controllerUnits}
     		</nav>
     	</section>
     );
